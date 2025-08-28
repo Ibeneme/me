234a0201d99e141ec7b2c9d76b828acae3a93faa
ibeneme <ikennaibenemee@gmail.com>
2025-08-28 15:33:29 +0100

    first commit

--- src/Pages/HowWeWorkPage.tsx
+++ src/Pages/HowWeWorkPage.tsx
@@ -1,10 +1,9 @@
 import FAQPage from "../Components/Faqs/FAQPage";
-import HowWeWorkComponent from "../Components/HowWeWorkComponent/HowWeWorkComponent";
 
 const HowWeWorkPage = () => {
   return (
     <div>
-      <HowWeWorkComponent />
+   
       <FAQPage />
     </div>
   );
